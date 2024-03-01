b62efa1f1a1f2869e656d9fa059c2db2e49a22a2
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-03-01 10:26:08 +0600

    Student interface created

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,60 @@
+export type Name = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type Parents = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type Guardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+// export type Address = {
+//   village?: string;
+//   area?: string;
+//   city: string;
+//   district: string;
+//   country: string;
+// };
+
+type BloodGroup =
+  | "A"
+  | "B"
+  | "AB"
+  | "O"
+  | "A+"
+  | "A-"
+  | "B+"
+  | "B-"
+  | "AB+"
+  | "AB-"
+  | "O+"
+  | "O-";
+
+export type Student = {
+  id: string;
+  name: Name;
+  gender: "male" | "female";
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: BloodGroup;
+  presentAddress?: string;
+  permanentAddress?: string;
+  parents: Parents;
+  guardian: Guardian;
+  avatar?: string;
+  isActive: "active" | "blocked";
+};
