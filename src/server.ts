174d1ff2f4effadedc31976a519c97a0ce5aebe4
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-03-07 19:58:45 +0600

    Credentials added

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,7 @@ main().catch((err) => console.log(err));
 app.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}`);
 });
+
+// NODE_ENV=development
+// PORT=5000
+// DATABASE_URL=mongodb+srv://pressflow:7aP2JWlcrpXMllmA@junior.tpsklbw.mongodb.net/pressflow?retryWrites=true&w=majority&appName=Junior
