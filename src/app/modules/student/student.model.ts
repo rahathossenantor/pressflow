087d56a773e60fda6bfee76727178d48a0e3a342
rahathossenantor <md.rahathossenantor@gmail.com>
2024-03-08 10:15:09 +0600

    Default validation implemented on student model

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -2,24 +2,24 @@ import { Schema, model } from "mongoose";
 import { Name, Parents, Guardian, Student } from "./student.interface";
 
 const nameSchema = new Schema<Name>({
-  firstName: { type: String, required: [true, "First name is required!"] },
+  firstName: { type: String, required: [true, "First name is required!"], minlength: 5, maxlength: 20 },
   middleName: { type: String },
-  lastName: { type: String, required: [true, "Last name is required!"] },
+  lastName: { type: String, required: [true, "Last name is required!"], minlength: 5, maxlength: 20 }
 });
 
 const parentsSchema = new Schema<Parents>({
-  fatherName: { type: String, required: true },
-  fatherOccupation: { type: String, required: true },
-  fatherContactNo: { type: String, required: true },
-  motherName: { type: String, required: true },
-  motherOccupation: { type: String, required: true },
-  motherContactNo: { type: String, required: true },
+  fatherName: { type: String, required: [true, "Father's name is required!"] },
+  fatherOccupation: { type: String, required: [true, "Father's occupation is required!"] },
+  fatherContactNo: { type: String, required: [true, "Father's contact number is required!"] },
+  motherName: { type: String, required: [true, "Mother's name is required!"] },
+  motherOccupation: { type: String, required: [true, "Mother's occupation is required!"] },
+  motherContactNo: { type: String, required: [true, "Mother's contact number is required!"] }
 });
 
 const guardianSchema = new Schema<Guardian>({
-  name: { type: String, required: true },
-  occupation: { type: String, required: true },
-  contactNo: { type: String, required: true },
+  name: { type: String, required: [true, "Guardin's name is required!"] },
+  occupation: { type: String, required: [true, "Guardin's occupation is required!"] },
+  contactNo: { type: String, required: [true, "Guardian's contact number is required!"] }
 });
 
 const studentSchema = new Schema<Student>({
@@ -28,7 +28,7 @@ const studentSchema = new Schema<Student>({
   gender: {
     type: String,
     enum: ["male", "famale", "other"],
-    required: true,
+    required: true
   },
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
@@ -49,7 +49,7 @@ const studentSchema = new Schema<Student>({
       "AB-",
       "O+",
       "O-",
-    ],
+    ]
   },
   presentAddress: { type: String },
   permanentAddress: { type: String, required: true },
@@ -59,8 +59,8 @@ const studentSchema = new Schema<Student>({
   isActive: {
     type: String,
     enum: ["active", "blocked"],
-    default: "active",
-  },
+    default: "active"
+  }
 });
 
 const StudentModel = model<Student>("student", studentSchema);
